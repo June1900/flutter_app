fde626c58210850be9d3e4613a98a2d0cdbc17e3
junzhangt <zkitun@163.com>
2019-07-20 16:18:39 +0800

    第一个Flutter示例程序

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,115 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    final titleSection = _TitleSection(
+        'Oeschinen Lake Campground', 'Kandersteg, Switzerland', '41');
+    final buttonSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildButtonColumn(context, Icons.call, 'CALL'),
+        _buildButtonColumn(context, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(context, Icons.share, 'SHARE'),
+      ],
+    ));
+
+    final textSection = Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        """
+        nihao jjjjdkdsfdjkdsakdsafksakdf
+        """,
+        softWrap: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+
+    return MaterialApp(
+      title: 'Flutter App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter'),
+          ),
+          body: ListView(
+            children: <Widget>[
+              Image.asset(
+                'images/like.jpg',
+                width: 600,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textSection
+            ],
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _TitleSection extends StatelessWidget {
   final String title;
+  final String subTitle;
+  final String startCount;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  _TitleSection(this.title, this.subTitle, this.startCount);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Container(
+      padding: EdgeInsets.all(30.2),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  subTitle,
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.w400),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text(startCount)
+        ],
       ),
     );
   }
+}
+
+Widget _buildButtonColumn(BuildContext context, IconData icon, String label) {
+  final color = Theme.of(context).primaryColor;
+  return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          padding: EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                color: color, fontSize: 15, fontWeight: FontWeight.normal),
+          ),
+        )
+      ]);
 }
